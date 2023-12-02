02e8ecef3a14ffd52df53d093947cbda9d6d5d3d
rogelio ramirez carmona <rogelios-1992@hotmail.com>
2023-12-02 00:31:47 -0500

    adding create new user

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native'
 import awsconfig from './src/aws-exports'
+import { useEffect } from 'react';
+import { getUser } from './src/graphql/queries'
+import { createUser } from './src/graphql/mutations'
 
-Amplify.configure({...awsconfig, Analytics: {disabled: true}});
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 function App() {
+
+  useEffect(() => {
+    const asyncUser = async () => {
+
+      const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true })
+
+      const userData = await API.graphql(graphqlOperation(getUser, { id: authUser.attributes.sub }))
+
+      if (userData.data.getUser) {
+        console.log('Existe en la base de datos')
+        return
+      }
+
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: 'Hey! im using WhatasApp'
+      }
+
+      if (!userData.data.getUser) {
+        await API.graphql(graphqlOperation(createUser, { input: newUser }))
+        return
+      }
+    }
+
+    asyncUser()
+
+  }, [])
+
   return (
     <View style={styles.container}>
       {/* <ChatsScreen /> */}
